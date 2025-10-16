b6be9dca331347b692126c8dba83ae0bfdcc227c
Diwice <coldwice@gmail.com>
2025-10-16 16:46:38 +0300

    Finished helper functions. Added some conversions. UNTESTED.

--- pkg/colorspace/colorspace.go
+++ pkg/colorspace/colorspace.go
@@ -5,6 +5,9 @@ import (
 	"fmt"
 )
 
+/* Since I'm stumbling on padding anyways, decided to use float64 data types instead.
+They produce more precise values, which is crucial when working with colors... */
+
 type RGB_obj struct {
 	RED uint8
 	GREEN uint8
@@ -36,33 +39,104 @@ type CIELAB_obj struct {
 	b float64
 }
 
+// Helper functions. Made for "DRY".
+
 func round_to_two_digits(some_float float64) float64 {
 	return math.Round(some_float*math.Pow10(2)) / math.Pow10(2)
 }
 
-func (o RGB_obj) get_normalized_values() (n_r, n_g, n_b float64) {
-	n_r, n_g, n_b = float64(o.RED)/255.0, float64(o.GREEN)/255.0, float64(o.BLUE)/255.0
-	return n_r, n_g, n_b
-}
-
 func hex_format(hex string) string {
-	//
+	if len(hex) < 2 {
+		return "0" + hex
+	}
+
+	return hex
 }
 
-func sector_formatting(sector, chr, ie float64) (float64, float64, float64) {
-	//
+// Both HSL/HSV to RGB are using the same algorithm
+
+func sector_formatting(sector, chr, ie float64) (float64, float64, float64, error) {
+	var n_r, n_g, n_b float64
+
+	if n_hue_sector := sector/100.0; n_hue_sector >= 0 && n_hue_sector < 1 {
+		n_r = chr
+		n_g = ie
+		n_b = 0.0
+	} else if n_hue_sector >= 1 && n_hue_sector < 2 {
+		n_r = ie
+		n_g = chr
+		n_b = 0.0
+	} else if n_hue_sector >= 2 && n_hue_sector < 3 {
+		n_r = 0.0
+		n_g = chr
+		n_b = ie
+	} else if n_hue_sector >= 3 && n_hue_sector < 4 {
+		n_r = 0.0
+		n_g = ie
+		n_b = chr
+	} else if n_hue_sector >= 4 && n_hue_sector < 5 {
+		n_r = ie
+		n_g = 0.0
+		n_b = chr
+	} else if n_hue_sector >= 5 && n_hue_sector < 6 {
+		n_r = chr
+		n_g = 0.0
+		n_b = ie
+	} else {
+		return 0.0, 0.0, 0.0, fmt.Errorf("Sector out of range : %f", n_hue_sector)
+	}
+
+	return n_r, n_g, n_b, nil
 }
 
-func norm_formatting(a, b, c float64) (uint8, uint8, uint8) {
-	//
+/* new_formatting INCLUDES gamut clipping.
+Both HSV and HSL spaces hold more color information than RGB space does.
+Causes data color loss. Not like I could directly convert some color spaces to others. */
+
+func new_formatting(a, b, c float64) (uint8, uint8, uint8) {
+	if a > 255.0 {
+		a = 255.0
+	} else {
+		a = math.Round(a)
+	}
+
+	if b > 255.0 {
+		b = 255.0
+	} else {
+		b = math.Round(b)
+	}
+
+	if c > 255.0 {
+		c = 255.0
+	} else {
+		c = math.Round(c)
+	}
+
+	return a, b, c
 }
 
 func cie_func(tp_val float64) float64 {
-	//
+	var sub_val float64
+
+	if tp_val > 0.008856 {
+		sub_val = math.Pow(tp_val, 1.0/3.0)
+	} else {
+		sub_val = (tp_val/7.787037037037037) + (0.13793103448275862)
+	}
+
+	return float64
 }
 
 func reverse_cie_func(ti_val float64) float64 {
-	//
+	var sub_val float64
+
+	if ti_val > 0.206897 {
+		sub_val = math.Pow(ti_val, 3.0)
+	} else {
+		sub_val = 0.12841854934601665*(ti_val - 0.13793103448275862)
+	}
+
+	return float64
 }
 
 func gamut_clip(val float64) float64 {
@@ -75,9 +149,36 @@ func gamut_clip(val float64) float64 {
 	return val
 }
 
-func linear_rgb_inverse(linear_val float64) float64 {
-	//
+func norm_to_linear(norm_val float64) float64 {
+	var linear_color float64
+
+	if norm_val <= 0.04045 {
+		linear_color = norm_val/12.92
+	} else {
+		linear_color = math.Pow(((norm_val + 0.055)/1.055), 2.4)
+	}
+
+	return linear_color
 }
+
+func linear_color_inverse(linear_val float64) float64 {
+	var inversed_color float64
+
+	if linear_val >= 0.0031308 {
+		inversed_color = 12.92*linear_val
+	} else {
+		inversed_color = 1.055*(math.Pow(linear_val, 1.0/2.4) - 0.055)
+	}
+
+	return inversed_color
+}
+
+func (o RGB_obj) get_normalized_values() (n_r, n_g, n_b float64) {
+	n_r, n_g, n_b = float64(o.RED)/255.0, float64(o.GREEN)/255.0, float64(o.BLUE)/255.0
+	return n_r, n_g, n_b
+}
+
+// Not gonna explain anything but RGB to CIE Lab since those are basic conversions.
 
 func (o RGB_obj) To_cmyk() CMYK_obj {
 	norm_r, norm_g, norm_b := o.get_normalized_values()
@@ -107,7 +208,6 @@ func (o RGB_obj) To_cmyk() CMYK_obj {
 	}
 
 	return new_cmyk_obj
-
 }
 
 func (o RGB_obj) To_hsl() HSL_obj {
@@ -199,62 +299,39 @@ func (o RGB_obj) To_hsv() HSV_obj {
 	return new_hsv_obj
 }
 
+/* Rough outline on RGB to CIE Lab algorithm :
+RGB -> normalized RGB -> linear RGB -> CIE XYZ D65 -> CIE XYZ D50 ->
+White Point to XYZ ratio -> CIE function conversions on ratios -> CIE Lab */
+
 func (o RGB_obj) To_cielab() CIELAB_obj {
 	norm_r, norm_g, norm_b := o.get_normalized_values()
-	var linear_r, linear_g, linear_b float64
 
-	if norm_r <= 0.04045 {
-		linear_r = norm_r/12.92
-	} else {
-		linear_r = math.Pow(((norm_r + 0.055)/1.055),2.4)
-	}
-
-	if norm_g <= 0.04045 {
-		linear_g = norm_g/12.92
-	} else {
-		linear_g = math.Pow(((norm_g + 0.055)/1.055),2.4)
-	}
-
-	if norm_b <= 0.04045 {
-		linear_b = norm_b/12.92
-	} else {
-		linear_b = math.Pow(((norm_b + 0.055)/1.055),2.4)
-	}
-
+	linear_r, linear_g, linear_b := norm_to_linear(norm_r), norm_to_linear(norm_g), norm_to_linear(norm_b)
+	/* Numbers which are used in those multiplications come from mathematical definition of RGB color space.
+	They are a part of matrix with coefficients used for RGB -> CIE XYZ conversions */
 	cie_x := (linear_r*0.4124564321) + (linear_g*0.3575760771) + (linear_b*0.1804374825)
 	cie_y := (linear_r*0.2126729074) + (linear_g*0.7151521631) + (linear_b*0.0721749293)
 	cie_z := (linear_r*0.0193338956) + (linear_g*0.1191920199) + (linear_b*0.9503039864)
-	// idfc if you want d65 cielab values. IT SHOULD BE IN D50
-	x_d, y_d, z_d := 0.964212, 1.000000, 0.825188
 
+	/* Further calculations mutate XYZ D65 values to XYZ D50 values, since RGB is defined relatively to D65.
+	If you want D65 CIE Lab values - remove everything up to adapt values and replace d-values.
+	Then replace adapt values with cie values in ratio values.*/
+
+	// White Point D50 values
+	x_d, y_d, z_d := 0.964212, 1.000000, 0.825188
+	// Adapting XYZ D65 to D50. Bradford CAT matrix values.
 	adapt_x := (cie_x*1.0478112) + (cie_y*0.0228866) + (cie_z*-0.0501270)
 	adapt_y := (cie_x*0.0295424) + (cie_y*0.9904844) + (cie_z*-0.0170491)
 	adapt_z := (cie_x*-0.0092345) + (cie_y*0.0150436) + (cie_z*0.7521316)
 
 	x_ratio, y_ratio, z_ratio := adapt_x/x_d, adapt_y/y_d, adapt_z/z_d
 
-	if x_ratio > 0.008856 {
-		x_ratio = math.Pow(x_ratio, 1.0/3.0)
-	} else {
-		x_ratio = (x_ratio/7.787037037037037) + (0.13793103448275862)
-	}
-
-	if y_ratio > 0.008856 {
-		y_ratio = math.Pow(y_ratio, 1.0/3.0)
-	} else {
-		y_ratio = (y_ratio/7.787037037037037) + (0.13793103448275862)
-	}
-
-	if z_ratio > 0.008856 {
-		z_ratio = math.Pow(z_ratio, 1.0/3.0)
-	} else {
-		z_ratio = (z_ratio/7.787037037037037) + (0.13793103448275862)
-	}
+	final_x, final_y, final_z := cie_func(x_ratio), cie_func(y_ratio), cie_func(z_ratio)
 
 	new_cielab_obj := CIELAB_obj{
-		L: 116.0*y_ratio - 16.0,
-		a: 500.0*(x_ratio - y_ratio),
-		b: 200.0*(y_ratio - z_ratio),
+		L: 116.0*final_y - 16.0,
+		a: 500.0*(final_x - final_y),
+		b: 200.0*(final_y - final_z),
 	}
 
 	new_cielab_obj.L = round_to_two_digits(new_cielab_obj.L)
@@ -267,19 +344,9 @@ func (o RGB_obj) To_cielab() CIELAB_obj {
 func (o RGB_obj) To_hex() string {
 	var res_hex string
 
-	n_r, n_g, n_b := fmt.Sprintf("%X", o.RED), fmt.Sprintf("%X", o.GREEN), fmt.Sprintf("%X", o.BLUE)
+	hex_r, hex_g, hex_b := fmt.Sprintf("%X", o.RED), fmt.Sprintf("%X", o.GREEN), fmt.Sprintf("%X", o.BLUE)
 
-	if len(n_r) < 2 {
-		n_r = "0" + n_r
-	}
-
-	if len(n_g) < 2 {
-		n_g = "0" + n_g
-	}
-
-	if len(n_b) < 2 {
-		n_b = "0" + n_b
-	}
+	hex_r, hex_g, hex_b = hex_format(hex_r), hex_format(hex_g), hex_format(hex_b)
 
 	res_hex = "#"+n_r+n_g+n_b
 
@@ -300,16 +367,32 @@ func (o CMYK_obj) To_rgb() RGB_obj {
 	return new_rgb_obj
 }
 
+/* Couldn't find direct conversion formulas myself.
+For some of the further transformations RGB is used as an intermidiate value.
+Color data is getting lost in process. */
+
 func (o CMYK_obj) To_hsl() HSL_obj {
-	//
+	new_rgb_obj := o.To_rgb()
+
+	new_hsl_obj := new_rgb_obj.To_hsl()
+
+	return new_hsl_obj
 }
 
 func (o CMYK_obj) To_hsv() HSV_obj {
-	//
+	new_rgb_obj := o.To_rgb()
+
+	new_hsv_obj := new_rgb_obj.To_hsv()
+
+	return new_hsv_obj
 }
 
 func (o CMYK_obj) To_cielab() CIELAB_obj {
-	//
+	new_rgb_obj := o.To_rgb()
+
+	new_cielab_obj := new_rgb_obj.To_cielab()
+
+	return new_cielab_obj
 }
 
 func (o HSL_obj) To_rgb() RGB_obj {
@@ -319,67 +402,36 @@ func (o HSL_obj) To_rgb() RGB_obj {
 	ie_value := chroma*(1.0 - math.Abs(math.Mod(hue_sector, 2.0) - 1.0))
 	var norm_r, norm_g, norm_b float64
 
-	if n_hue_sector := hue_sector/100.0; n_hue_sector >= 0 && n_hue_sector < 1 {
-		norm_r = chroma
-		norm_g = ie_value
-		norm_b = 0.0
-	} else if n_hue_sector >= 1 && n_hue_sector < 2 {
-		norm_r = ie_value
-		norm_g = chroma
-		norm_b = 0.0
-	} else if n_hue_sector >= 2 && n_hue_sector < 3 {
-		norm_r = 0.0
-		norm_g = chroma
-		norm_b = ie_value
-	} else if n_hue_sector >= 3 && n_hue_sector < 4 {
-		norm_r = 0.0
-		norm_g = ie_value
-		norm_b = chroma
-	} else if n_hue_sector >= 4 && n_hue_sector < 5 {
-		norm_r = ie_value
-		norm_g = 0.0
-		norm_b = chroma
-	} else if n_hue_sector >= 5 && n_hue_sector < 6 {
-		norm_r = chroma
-		norm_g = 0.0
-		norm_b = ie_value
+	if sub_r, sub_g, sub_b, err := sector_formatting(hue_sector, chroma, ie_value); err == nil {
+		norm_r = sub_r
+		norm_g = sub_g
+		norm_b = sub_b
 	} else {
-		fmt.Println("Hue Sector is >= 6 or < 0 -", n_hue_sector)
+		fmt.Printf("Error converting HSL to RGB : %ss / %s\n", err)
+		return RGB_obj{RED: 0.0, GREEN: 0.0, BLUE: 0.0}
 	}
 
 	l_adjust := o.LIGHTNESS - (chroma/2)
 
-	new_red, new_green, new_blue := l_adjust + norm_r, l_adjust + norm_g, l_adjust + norm_b
+	sub_red, sub_green, sub_blue := l_adjust + norm_r, l_adjust + norm_g, l_adjust + norm_b
 
-	if new_red > 255.0 {
-		new_red = 255.0
-	} else {
-		new_red = math.Round(new_red)
-	}
-
-	if new_green > 255.0 {
-		new_green = 255.0
-	} else {
-		new_green = math.Round(new_green)
-	}
-
-	if new_blue > 255.0 {
-		new_blue = 255.0
-	} else {
-		new_blue = math.Round(new_blue)
-	}
+	new_red, new_green, new_blue := new_formatting(sub_red, sub_green, sub_blue)
 
 	new_rgb_obj := RGB_obj{
-		RED: uint8(new_red),
-		GREEN: uint8(new_green),
-		BLUE: uint8(new_blue),
+		RED: new_red,
+		GREEN: new_green,
+		BLUE: new_blue,
 	}
 
 	return new_rgb_obj
 }
 
 func (o HSL_obj) To_cmyk() CMYK_obj {
-	//
+	new_rgb_obj := o.To_rgb()
+
+	new_cmyk_obj := new_rgb_obj.To_cmyk()
+
+	return new_cmyk_obj
 }
 
 func (o HSL_obj) To_hsv() HSV_obj {
@@ -387,7 +439,11 @@ func (o HSL_obj) To_hsv() HSV_obj {
 }
 
 func (o HSL_obj) To_cielab() CIELAB_obj {
-	//
+	new_rgb_obj := o.To_rgb()
+
+	new_cielab_obj := new_rgb_obj.To_cielab()
+
+	return new_cielab_obj
 }
 
 func (o HSV_obj) To_rgb() RGB_obj {
@@ -397,67 +453,36 @@ func (o HSV_obj) To_rgb() RGB_obj {
 	ie_value := chroma*(1.0 - math.Abs(math.Mod(hue_sector, 2.0) - 1.0))
 	var norm_r, norm_g, norm_b float64
 
-	if f_sector := math.Floor(hue_sector/100.0); f_sector == 0.0 {
-		norm_r = chroma
-		norm_g = ie_value
-		norm_b = 0.0
-	} else if f_sector == 1.0 {
-		norm_r = ie_value
-		norm_g = chroma
-		norm_b = 0.0
-	} else if f_sector == 2.0 {
-		norm_r = 0.0
-		norm_g = chroma
-		norm_b = ie_value
-	} else if f_sector == 3.0 {
-		norm_r = 0.0
-		norm_g = ie_value
-		norm_b = chroma
-	} else if f_sector == 4.0 {
-		norm_r = ie_value
-		norm_g = 0.0
-		norm_b = chroma
-	} else if f_sector == 5.0 {
-		norm_r = chroma
-		norm_g = 0.0
-		norm_b = ie_value
+	if sub_r, sub_g, sub_b, err := sector_formatting(hue_sector, chroma, ie_value); err == nil {
+		norm_r = sub_r
+		norm_g = sub_g
+		norm_b = sub_b
 	} else {
-		fmt.Println("Sector > 5 or < 0; -", f_sector)
+		fmt.Printf("Error converting HSL to RGB : %ss / %s\n", err)
+		return RGB_obj{RED: 0.0, GREEN: 0.0, BLUE: 0.0}
 	}
 
 	l_adjust := o.VALUE - chroma
 
-	new_red, new_green, new_blue := l_adjust + norm_r, l_adjust + norm_g, l_adjust + norm_b
+	sub_red, sub_green, sub_blue := l_adjust + norm_r, l_adjust + norm_g, l_adjust + norm_b
 
-	if new_red > 255.0 {
-		new_red = 255.0
-	} else {
-		new_red = math.Round(new_red)
-	}
-
-	if new_green > 255.0 {
-		new_green = 255.0
-	} else {
-		new_green = math.Round(new_green)
-	}
-
-	if new_blue > 255.0 {
-		new_blue = 255.0
-	} else {
-		new_blue = math.Round(new_blue)
-	}
+	new_red, new_green, new_blue := new_formatting(sub_red, sub_green, sub_blue)
 
 	new_rgb_obj = RGB_obj{
-		RED: uint8(new_red),
-		GREEN: uint8(new_green),
-		BLUE: uint8(new_blue),
+		RED: new_red,
+		GREEN: new_green,
+		BLUE: new_blue,
 	}
 
 	return new_rgb_obj
 }
 
 func (o HSV_obj) To_cmyk() CMYK_obj {
-	//
+	new_rgb_obj := o.To_rgb()
+
+	new_cmyk_obj := new_rgb_obj.To_cmyk()
+
+	return new_cmyk_obj
 }
 
 func (o HSV_obj) To_hsl() HSL_obj {
@@ -465,31 +490,26 @@ func (o HSV_obj) To_hsl() HSL_obj {
 }
 
 func (o HSV_obj) To_cielab() CIELAB_obj {
-	//
+	new_rgb_obj := o.To_rgb()
+
+	new_cielab_obj := new_rgb_obj.To_cielab()
+
+	return new_cielab_obj
 }
+
+// CIE Lab (D50) to RGB algorithm is pretty much the same but inversed RGB to CIE Lab (D50) 
 
 func (o CIELAB_obj) To_rgb() RGB_obj {
 	x_d, y_d, z_d := 0.964212, 1.000000, 0.825188
 
-	var x_ratio, y_ratio, z_ratio float64
+	inv_a := (o.a/500.0) + ((o.L + 16.0)/116.0)
+	x_ratio := reverse_cie_func(inv_a)
 
-	if inv_a := (o.a/500.0) + ((o.L + 16.0)/116.0); inv_a > 0.206897 {
-		x_ratio = math.Pow(inv_a, 3.0)
-	} else {
-		x_ratio = 0.12841854934601665*(inv_a - 0.13793103448275862)
-	}
+	inv_l := (o.L + 16.0)/116.0
+	y_ratio := reverse_cie_func(inv_l)
 
-	if inv_l := (o.L + 16.0)/116.0; inv_l > 0.206897 {
-		y_ratio = math.Pow(inv_l, 3.0)
-	} else {
-		y_ratio = 0.12841854934601665*(inv_l - 0.13793103448275862)
-	}
-
-	if inv_b := ((o.L + 16.0)/116.0) - (o.b/200.0) ; inv_b > 0.206897 {
-		z_ratio = math.Pow(inv_b, 3.0)
-	} else {
-		z_ratio = 0.12841854934601665*(inv_b - 0.13793103448275862)
-	}
+	inv_b := ((o.L + 16.0)/116.0) - (o.b/200.0)
+	z_ratio := reverse_cie_func(inv_b)
 
 	adapt_x, adapt_y, adapt_z := x_ratio*x_d, y_ratio*y_d, z_ratio*z_d
 
@@ -503,25 +523,7 @@ func (o CIELAB_obj) To_rgb() RGB_obj {
 
 	linear_r, linear_g, linear_b = gamut_clip(linear_r), gamut_clip(linear_g), gamut_clip(linear_b)
 
-	var new_red, new_green, new_blue float64
-
-	if linear_r >= 0.0031308 {
-		new_red = 12.92*linear_r
-	} else {
-		new_red = 1.055*(math.Pow(linear_r, 1.0/2.4) - 0.055)
-	}
-
-	if linear_g >= 0.0031308 {
-		new_green = 12.92*linear_g
-	} else {
-		new_green = 1.055*(math.Pow(linear_g, 1.0/2.4) - 0.055)
-	}
-
-	if linear_b >= 0.0031308 {
-		new_blue = 12.92*linear_b
-	} else {
-		new_blue = 1.055*(math.Pow(linear_b, 1.0/2.4) - 0.055)
-	}
+	new_red, new_green, new_blue := linear_rgb_inverse(linear_r), linear_rgb_inverse(linear_g), linear_rgb_inverse(linear_b)
 
 	new_rgb_obj := RGB_obj{
 		RED: uint8(math.Round(new_red*255.0)),
@@ -533,13 +535,25 @@ func (o CIELAB_obj) To_rgb() RGB_obj {
 }
 
 func (o CIELAB_obj) To_cmyk() CMYK_obj {
-	//
+	new_rgb_obj := o.To_rgb()
+
+	new_cmyk_obj := new_rgb_obj.To_cmyk()
+
+	return new_cmyk_obj
 }
 
 func (o CIELAB_obj) To_hsl() HSL_obj {
-	//
+	new_rgb_obj := o.To_rgb()
+
+	new_hsl_obj := new_rgb_obj.To_hsl()
+
+	return new_hsl_obj
 }
 
 func (o CIELAB_obj) To_hsv() HSV_obj {
-	//
+	new_rgb_obj := o.To_rgb()
+
+	new_hsv_obj := new_rgb_obj.To_hsv()
+
+	return new_hsv_obj
 }
