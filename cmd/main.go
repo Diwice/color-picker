49fc724378d4b44d5e7912135857c9b71adec2de
Diwice <coldwice@gmail.com>
2025-10-19 16:37:14 +0300

    Fyne testing

--- cmd/main.go
+++ cmd/main.go
@@ -2,73 +2,22 @@ package main
 
 import (
 	"fmt"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/widget"
 	"github.com/Diwice/color-picker/pkg/colorspace"
 	)
 
 func main() {
-	cmyk := colorspace.CMYK_obj{
-		CYAN: ,
-		MAGENTA: ,
-		YELLOW: ,
-		KEY: ,
+	my_color := colorspace.RGB_obj{
+		RED: 255,
+		GREEN: 0,
+		BLUE: 0,
 	}
-	rgb_zero := cmyk.To_rgb()
-	fmt.Print("---CMYK regular---\n", cmyk, "\n", rgb_zero)
-	
-	hsv := colorspace.HSV_obj{
-		HUE: ,
-		SATURATION: ,
-		VALUE: ,
-	}
-	rgb_one := hsv.To_rgb()
-	fmt.Print("\n---HSV regular---\n", hsv, "\n", rgb_one)
 
-	hsl := colorspace.HSL_obj{
-		HUE: ,
-		SATURATION: ,
-		LIGHTNESS: ,
-	}
-	rgb_two := hsl.To_rgb()
-	fmt.Print("\n---HSL regular---\n", hsl, "\n", rgb_two)
+	my_app := app.New()
+	my_window := my_app.NewWindow("Hello")
+	my_window.SetContent(widget.NewLabel(fmt.Sprintf("The color you've made : R-%d/G-%d/B-%d", my_color.RED, my_color.GREEN, my_color.BLUE))
 
-	cielab := colorspace.CIELAB_obj{
-		L: ,
-		A: ,
-		B: ,
-	}
-	rgb_three := cielab.To_rgb()
-	fmt.Print("\n---CIE Lab regular---\n", cielab, "\n", rgb_three)
-
-	cmyk_gamut := colorspace.CMYK_obj{
-		CYAN: ,
-		MAGENTA: ,
-		YELLOW: ,
-		KEY: ,
-	}
-	rgb_gamut_zero := cmyk_gamut.To_rgb()
-	fmt.Print("\n---CMYK with gamut---\n", cmyk_gamut, "\n", rgb_gamut_zero)
-
-	cielab_gamut := colorspace.CIELAB_obj{
-		L: 94.0,
-		A: 128.0,
-		B: 128.0,
-	}
-	rgb_gamut_one := cielab_gamut.To_rgb()
-	fmt.Print("\n---CIE Lab with gamut---\n", cielab_gamut, "\n", rgb_gamut_one)
-
-	hsv_to := colorspace.HSV_obj{
-		HUE: ,
-		SATURATION: ,
-		VALUE: ,
-	}
-	hsl_from := hsv_to.To_hsl()
-	fmt.Print("\n---HSV to HSL---\n", hsv_to, "\n", hsl_from)
-
-	hsl_to := colorspace.HSL_obj{
-		HUE: ,
-		SATURATION: ,
-		VALUE: ,
-	}
-	hsv_from := hsl_to.To_hsv()
-	fmt.Print("\n---HSL to HSV---\n", hsl_to, "\n", hsv_from)
+	my_window.Show()
+	my_app.Run()
 }
